8cc248765ab5b72acfa4537d7db0c5846cb7d264
Christopher Waldon <christopher.waldon.dev@gmail.com>
2016-09-07 20:28:06 -0400

    Refactor to reduce redundancy

--- main.go
+++ main.go
@@ -19,49 +19,41 @@ func main() {
 	if err != nil {
 		log.Println(red(err))
 	}
-	fmt.Println(blue(pluginDir) + " targeted")
+	fmt.Println(blue(pluginDir.Name()) + " targeted")
 	vimDir, err := findVimPluginDir()
 	if err != nil {
 		log.Println(red(err))
 	}
-	fmt.Println(blue(vimDir) + " targeted")
+	fmt.Println(blue(vimDir.Name()) + " targeted")
+}
+
+// getDirectory returns the directory at the given path if it can be opened
+// and nil with an error otherwise.
+func getDirectory(path) (*os.File, error) {
+	directory, err := os.Open(bundlePath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Unable to find %s directory:", directory)
+	}
+	return directory, err
 }
 
 // findNvimPluginDir returns the directory in which neovim stores its plugins
-// or the empty string if it was unable to find the directory
-func findNvimPluginDir() (string, error) {
+// or the empty string if it was unable to find the directory.
+func findNvimPluginDir() (*os.File, error) {
 	xdgHome := os.Getenv("XDG_CONFIG_HOME")
 	if len(xdgHome) < 1 {
-		fmt.Println(yellow("Environment Variable XDG_CONFIG_HOME" +
-			" is not set or has no value. Please set this if" +
-			" you use neovim. Inferring default value for" +
-			" XDG_CONFIG_HOME..."))
+		fmt.Println(yellow("$XDG_CONFIG_HOME undefined. Inferring default value..."))
 		xdgHome = path.Join(os.Getenv("HOME"), ".config")
 	}
 
-	fmt.Println("XDG_CONFIG_HOME=" + xdgHome)
 	bundlePath := path.Join(xdgHome, "nvim", "bundle")
-	bundleDir, err := os.Open(bundlePath)
-	if err != nil {
-		return "", errors.Wrapf(err, "Unable to find %s directory:", bundlePath)
-	}
-	defer bundleDir.Close()
-	fmt.Println(green(bundlePath + " exists"))
-	return bundlePath, nil
+	return getDirectory(bundlePath)
 }
 
 // findNvimPluginDir returns the directory in which neovim stores its plugins
-// or the empty string if it was unable to find the directory
-func findVimPluginDir() (string, error) {
+// or the empty string if it was unable to find the directory.
+func findVimPluginDir() (*os.File, error) {
 	home := os.Getenv("HOME")
-	fmt.Println("HOME=" + home)
-
 	bundlePath := path.Join(home, ".vim", "bundle")
-	bundleDir, err := os.Open(bundlePath)
-	if err != nil {
-		return "", errors.Wrapf(err, "Unable to find %s directory:", bundlePath)
-	}
-	defer bundleDir.Close()
-	fmt.Println(green(bundlePath + " exists"))
-	return bundlePath, nil
+	return getDirectory(bundlePath)
 }
