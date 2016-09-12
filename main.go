af143f641b062876796fda11f43e226d0e4470e1
Christopher Waldon <christopher.waldon.dev@gmail.com>
2016-09-11 20:11:59 -0400

    Detect git repositories

--- main.go
+++ main.go
@@ -46,6 +46,57 @@ func main() {
 		os.Exit(1)
 	}
 	fmt.Println(blue(pluginDir.Name()) + " targeted")
+
+	// Find plugin git repositories
+	pluginCandidates, err := pluginDir.Readdir(0)
+	if err != nil {
+		panic(errors.Wrapf(err, "Unable to read the contents of plugin directory %s", pluginDir.Name()))
+	}
+	plugins := make([]string, 0)
+	for _, candidate := range pluginCandidates {
+		fullPath := pluginDir.Name() + string(os.PathSeparator) + candidate.Name()
+		fmt.Printf("Checking %15s...", candidate.Name())
+		if ok, err := isGitRepository(fullPath); err != nil {
+			fmt.Fprintln(os.Stderr, yellow(err))
+		} else if ok {
+			plugins = append(plugins, fullPath)
+			fmt.Println(green("OK"))
+		} else {
+			fmt.Println(red("NO"))
+		}
+	}
+	fmt.Printf(green("%d plugins found\n"), len(plugins))
+	numberPlugins := len(plugins)
+	updatesCompleted := make(chan int, 0)
+	for _, repository := range plugins {
+		go updateGitRepo(repository, updatesCompleted)
+	}
+	for i := 0; i < numberPlugins; i++ {
+		<-updatesCompleted
+	}
+	fmt.Println(green("Plugins updated."))
+
+}
+
+// isGitRepository checks whether a path points to the root directory of a git
+// repository.
+func isGitRepository(path string) (bool, error) {
+	candidateDir, err := getDirectory(path)
+	defer candidateDir.Close()
+	if err != nil {
+		return false, errors.Wrapf(err, "Unable to determine if %s is a git repository", path)
+	}
+	contents, err := candidateDir.Readdir(0)
+	if err != nil {
+		return false, errors.Wrapf(err, "Unable to determine if %s is a git repository", path)
+	}
+	for _, file := range contents {
+		if file.Name() == ".git" {
+			return true, nil
+		}
+	}
+	return false, nil
+
 }
 
 // printUsage prints usage information to stderr and exits with an error status.
